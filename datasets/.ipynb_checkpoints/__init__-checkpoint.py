07fd23a6f99ca2960e894554d72ba69fdaff17f5
GongCheng-gpuserver-100 <gongcheng_nankai@163.com>
2021-09-03 08:03:33 +0000

    add ignore

--- datasets/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,2 +0,0 @@
-from . import imagenet 
-from .base import DataLoaderX, data_prefetcher
--- quantizers/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-# from . import diff_tricks,quantization,fixedq
-from .diff_tricks import STE, PWL, PWL_C, get_diff_func
-from .quantization import Quantization
-from .fixedq import FixedQuant,FixedQuantAct
