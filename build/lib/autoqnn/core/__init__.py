7567b3c552d73a1f6270a051955b83be197d7803
GongCheng-gpuserver-100 <gongcheng_nankai@163.com>
2021-09-27 13:49:39 +0000

    remove useless folders

--- build/lib/autoqnn/core/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import module_convert
-from .module_convert import convert, _remove_qconfig
--- build/lib/autoqnn/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# from . import generic_utils, module_utils
-from .generic_utils import EMA, zero_grad
-from .module_utils import view_module
